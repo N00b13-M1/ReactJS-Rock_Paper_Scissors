b11c4f447e68dd3ea2aad271f76d4ae3447e3d61
N00b13-M1 <michael.cornelis@gmail.com>
2022-02-14 10:45:19 +0100

    CommitCSS

--- src/components/Body.js
+++ src/components/Body.js
@@ -29,6 +29,42 @@ class Body extends Component {
         console.log(choix);
     }
 
+    validate = () => {
+        let possibilite = ["paper", "rock", "scissors"]
+        let stateCopy = { ...this.state }
+        stateCopy.choixOrdi = possibilite[Math.floor(Math.random() * possibilite.length)]
+        if (stateCopy.choix === "paper" && stateCopy.choixOrdi === "rock") {
+            stateCopy.gagnant = "Michael"
+            stateCopy.score++
+        } else if (stateCopy.choix === "rock" && stateCopy.choixOrdi === "scissors") {
+            stateCopy.gagnant = "Michael"
+            stateCopy.score++
+        } else if (stateCopy.choix === "scissors" && stateCopy.choixOrdi === "paper") {
+            stateCopy.gagnant = "Michael"
+            stateCopy.score++
+        } else if (stateCopy.choixOrdi === "paper" && stateCopy.choix === "rock") {
+            stateCopy.gagnant = "Computer"
+            stateCopy.score--
+        } else if (stateCopy.choixOrdi === "rock" && stateCopy.choix === "scissors") {
+            stateCopy.gagnant = "Computer"
+            stateCopy.score--
+        } else if (stateCopy.choixOrdi === "scissors" && stateCopy.choix === "paper") {
+            stateCopy.gagnant = "Computer"
+            stateCopy.score--
+        } else {
+            stateCopy.gagnant = "Draw"
+        }
+
+        this.setState(stateCopy)
+    }
+    playAgain = () => {
+        let stateCopy = { ...this.state }
+        stateCopy.etape = "1"
+        stateCopy.gagnant = ""
+        stateCopy.choix = ""
+        stateCopy.choixOrdi = ""
+        this.setState(stateCopy)
+    }
 
     render() {
         return (
@@ -64,13 +100,13 @@ class Body extends Component {
             //         test
             //     </div>
             // </div>
-                    <div className="body row justify-content-center align-items-center">
-                        <div className="column1 col-lg-6 col-md-6 col-12 justify-content-center align-items-center text-center">
+                    <div className="body-2 container-fluid d-flex justify-content-center align-items-center">
+                        <div className="column1 col-6 justify-content-center align-items-center text-center px-2">
                             {
                                 this.state.choix === "paper" &&
 
-                            <div className="button border1">
-                                <button className="border paper rounded-circle" onClick={() => this.choix("paper")}>
+                            <div className="button border1 my-3">
+                                <button className="border paper rounded-circle">
                                     <img src="./img/icon-paper.svg" alt="paper" className="img-fluid" />
                                 </button>
                             </div>
@@ -78,28 +114,56 @@ class Body extends Component {
                             }
                             {
                                 this.state.choix=== "rock" &&
-                            <div className="button border2">
-                                <button className="border rock rounded-circle" onClick={() => this.choix("rock")}>
+                            <div className="button border2 my-3">
+                                <button className="border rock rounded-circle">
                                     <img src="./img/icon-rock.svg" alt="rock" className="img-fluid" />
                                 </button>
                             </div>
                             }
                             {
                                 this.state.choix === "scissors" &&
-                            <div className="button border3">
-                                <button className="border scissors rounded-circle" onClick={() => this.choix("scissors")}>
+                            <div className="button border3 my-3">
+                                <button className="border scissors rounded-circle">
                                     <img src="./img/icon-scissors.svg" alt="scissors" className="img-fluid" />
                                 </button>
                             </div>
                             }
-                            <h5>YOU PICKED</h5>
+                            <h5 className="my-3">YOU PICKED</h5>
                         </div>
-                        <div className="column2 col-lg-6 col-md-6 col-12 justify-content-center align-items-center text-center">
-                            <div className="circle-right">
+                    <div className="column2 col-6 justify-content-center align-items-center text-center px-2">
+                        {
+                            this.state.choixOrdi === "paper" &&
+                            <div className="button border1 my-3">
+                                <button className="border paper rounded-circle">
+                                    <img src="./img/icon-paper.svg" alt="paper" className="img-fluid" />
+                                </button>
                             </div>
-                            <h5>THE HOUSE PICKED</h5>
-                        </div>
+                        }
+                        {
+                            this.state.choixOrdi === "rock" &&
+                            <div className="button border2 my-3">
+                                <button className="border rock rounded-circle">
+                                    <img src="./img/icon-rock.svg" alt="rock" className="img-fluid" />
+                                </button>
+                            </div>
+                        }
+                        {
+                            this.state.choixOrdi === "scissors" &&
+                            <div className="button border3 my-3">
+                                <button className="border scissors rounded-circle">
+                                    <img src="./img/icon-scissors.svg" alt="scissors" className="img-fluid" />
+                                </button>
+                            </div>
+                        }
+                        {
+                            this.state.choixOrdi === "" &&
+                            <p></p>
+                        }
+                        <h5 className="my-3">THE HOUSE PICKED</h5>
                     </div>
+                    {this.state.choixOrdi === "" ?
+                    <button onClick={this.validate}>Validate</button>: <button onClick={this.playAgain}>Play again</button>}
+                </div>
                 }
         <Footer modal={this.state.modal} showModal={this.showModal}/>
 
